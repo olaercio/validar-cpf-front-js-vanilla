7e8b2697ab3fe2d1bb242b1755273a7b653232c4
Olaercio <olaerzin@hotmail.com>
2020-08-05 16:47:25 -0300

    Nova alteração

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://https://turma6c-itau-olaercio.herokuapp.com/validacpf?cpf="
+const URL="https://turma6c-itau-olaercio.herokuapp.com/validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
